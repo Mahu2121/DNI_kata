f82412d514c2544c37674d8990be575faed9f039
Hugo Gonzalez Besada <hugogbmahu@gmail.com>
2025-01-24 13:59:12 +0100

    feat:

--- src/dni.py
+++ src/dni.py
@@ -29,6 +29,52 @@ class Dni:
         return self.letra_sana
     
     def check_dni(self):
-        
+        self.numero_sano(self.__check_dni_lenght() and self.__check_dni_number())
+        return self.get_numero_sano()
+    
+    def check_cif(self)
+        return self.__check_dni_number and self.__check_letra()
+    
+    def check_letra(self):
+        if self.get_numero_sano():
+            self.letra_sana( self.__check_letra() and  self.__check_letra_mayuscula() and self.__check_letra_vale )
+            return self.get_letra_sana()
 
+        else:
+            return False
+            
+
+
+    def dar_letra(self):
+
+        if self.get_numero_sano():
+            return self.tabla.calcular_resto(self.__get_parte_numerica())
+        else:
+            return None
+
+
+
+
+    def __check_dni_lenght(self):
+        return len(self.get_dni()) == 9
+    
+    def __check_dni_number(self):
+        return self.dni[:-1].isdigit()
+    
+    def __check_letra(self):
+        return self.__get_parte_alfabetica_dni.isalpha()
+
+    def __get_parte_alfabetica_dni(self):
+        return self.dni[-1]
+
+    def __check_letra_mayuscula(self):
+        return self.__get_parte_alfabetica_dni.isupper()
+    
+    def __get_parte_numerica(self):
+        return self.get_dni[:-1]
+
+    def __check_letra_vale(self):
+
+        if self.get_numero_sano:
+            return self.__get_parte_alfabetica_dni() == self.dar_letra()
     
